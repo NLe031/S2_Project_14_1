9a1d59fe847224d228c486a81ef49b25124eab77
Nicholas Le <nle031@west-mec.org>
2019-04-03 16:34:53 -0700

    Final Commit
    Ready for Review.

--- na_styler.js
+++ na_styler.js
@@ -22,7 +22,7 @@
 */
 
 window.addEventListener("load", setStyles);
-
+//adds a random style sheet every time the page is loaded
 function setStyles() {
       var styleNum = randInt(5);
       var fancySheet = document.createElement("link")
@@ -34,20 +34,41 @@ function setStyles() {
       var figBox = document.createElement("figure");
       figBox.setAttribute("id", "styleThumbs");
       document.getElementById("box").appendChild(figBox);
-
+      //loops through the 5 imgs and style sheets
       for (var i = 0; i <= 4; i++) {
             var sheetImg = document.createElement("img");
-            sheetImg.setAttribute("src", "na_small_" + i + ".css")
-            sheetImg.setAttribute("alt", +i + ".css")
-            sheetImg.addEventListener(function () {
-                  //fancySheet.setAttribute("");
-            })
-            sheetImg.onclick = function (e) {
+            sheetImg.setAttribute("src", "na_small_" + i + ".png")
+            sheetImg.setAttribute("alt", "na_style_" + i + ".css")
+            sheetImg.addEventListener("click", function (e) {
                   document.getElementById("fancySheet").setAttribute("href", e.target.alt);
-            }
+            })
+
             figBox.appendChild(sheetImg);
       }
+      var thumbStyles = document.createElement("style");
+      document.head.appendChild(thumbStyles);
+      //adds the css styles into the index document so that the pictures are a the bottom
+      document.styleSheets[document.styleSheets.length - 1]
+            .insertRule(
+                  "figure#styleThumbs { \
+                  position: absolute; \
+                  left: 0px; \
+                  bottom: 0px; \
+            }", 0);
 
+      document.styleSheets[document.styleSheets.length - 1]
+            .insertRule(
+                  "figure#styleThumbs img { \
+                  outline: 1px solid black \
+                  cursor: pointer; \
+                  opacity: 0.75; \
+            }", 1);
+      document.styleSheets[document.styleSheets.length - 1]
+            .insertRule(
+                  "figure#styleThumbs img:hover { \
+                  outline: 1px solid red; \
+                  opacity: 1.0; \
+            }", 2);
 }
 
 
