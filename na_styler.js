7429da0974a07f2847c34defd62bce1832c580e7
Nicholas Le <nle031@west-mec.org>
2019-04-02 18:25:52 -0700

    Updated Files
    finished steps 1-9 which created the setstyles function that has a for loop and multiple nodes.

--- na_styler.js
+++ na_styler.js
@@ -21,6 +21,35 @@
 
 */
 
+window.addEventListener("load", setStyles);
+
+function setStyles() {
+      var styleNum = randInt(5);
+      var fancySheet = document.createElement("link")
+      fancySheet.setAttribute("rel", "styleSheet");
+      fancySheet.setAttribute("id", "fancySheet");
+      fancySheet.setAttribute("href", "na_style_" + styleNum + ".css");
+
+      document.head.appendChild(fancySheet);
+      var figBox = document.createElement("figure");
+      figBox.setAttribute("id", "styleThumbs");
+      document.getElementById("box").appendChild(figBox);
+
+      for (var i = 0; i <= 4; i++) {
+            var sheetImg = document.createElement("img");
+            sheetImg.setAttribute("src", "na_small_" + i + ".css")
+            sheetImg.setAttribute("alt", +i + ".css")
+            sheetImg.addEventListener(function () {
+                  //fancySheet.setAttribute("");
+            })
+            sheetImg.onclick = function (e) {
+                  document.getElementById("fancySheet").setAttribute("href", e.target.alt);
+            }
+            figBox.appendChild(sheetImg);
+      }
+
+}
+
 
 function randInt(size) {
       return Math.floor(size * Math.random());
